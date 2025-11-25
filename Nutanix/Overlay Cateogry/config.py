dabc05008e98ee6b138503852e8d852c6826742d
Ayush Srivastava <ayush.srivastava@nutanix.com>
2025-11-26 03:49:44 +0530

    Replace sensitive data with placeholders in config.py
    Updated sensitive information placeholders in config.py.

--- Nutanix/Overlay Cateogry/config.py
+++ Nutanix/Overlay Cateogry/config.py
@@ -1,13 +1,11 @@
-# config.py
-
 # Prism Central connection details
-PRISM_CENTRAL_IP = "10.38.78.39"
-USERNAME = "admin"
-PASSWORD = "nx2Tech973!"
+PRISM_CENTRAL_IP = "10.X.X.X" # Even FQDN can be used here
+USERNAME = "<username>"
+PASSWORD = "<password>"
 
 # Category details
-CATEGORY_NAME = "CUPA"
-CATEGORY_VALUE = "in a vpc"
+CATEGORY_NAME = "<category_name>"
+CATEGORY_VALUE = "<category_value>"
 
 # Default headers for REST API calls
 HEADERS = {
